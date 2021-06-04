816a9435333c65022798ae5a25eec03b56a3bcf2
Nick <1234lizhang@gmail.com>
2021-06-04 23:04:00 +0800

    Updated Category Second Page

--- script.js
+++ script.js
@@ -2,22 +2,27 @@
 
 
 var home = $("#Home");
-var category = $("#Category");
+var category1 = $("#Category1");
 var aboutUs = $("#About-Us");
+var category2 = $("#Category2");
 
 var homeBtn = $("#Home-Btn");
 var categoryBtn = $("#Category-Btn");
 var aboutUsBtn = $("#AboutUs-Btn");
+var categoryDown = $("#Category-Down");
+var categoryUp = $("#Category-Up");
+
 
 var selectLink = 0;
 
 function RemoveActive() {
-    category.removeClass("active");
+    category1.removeClass("active");
     aboutUs.removeClass("active");
     home.removeClass("active");
     categoryBtn.removeClass("active");
     aboutUsBtn.removeClass("active");
     homeBtn.removeClass("active");
+    category2.removeClass("active");
 }
 
 function AddActive(index) {
@@ -27,7 +32,7 @@ function AddActive(index) {
         homeBtn.addClass("active");
     }
     else if(index == 1) {
-        category.addClass("active");
+        category1.addClass("active");
         categoryBtn.addClass("active");
 
     }
@@ -35,6 +40,10 @@ function AddActive(index) {
         aboutUs.addClass("active");
         aboutUsBtn.addClass("active");
 
+    }
+    else if(index == 3) {
+        category2.addClass("active");
+        categoryBtn.addClass("active");
     }
 }
 
