bc0155d98f289650a64c84a92bf255c788d78aa2
Nick <1234lizhang@gmail.com>
2021-06-05 19:08:01 +0800

    Browsable page

---
+++ exhibition.js
@@ -0,0 +1,100 @@
+/// <reference path="./typings/globals/jquery/index.d.ts" />
+var weaponCard = $("#weapon-card-template").html();
+var weaponPage = $("#weapon-page-template").html();
+// var weaponPopUp = $("#weapon-pop-up-template").html();
+//come here to walk around
+
+var templateCard = Handlebars.compile(weaponCard);
+var templatePage = Handlebars.compile(weaponPage);
+
+
+// var templatePopUp = Handlebars.compile(weaponPopUp);
+
+
+var weapons = [
+    {
+        name: "Black Sword",
+        imageUrl: "./imgs/weaponSample.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nihil velit consequatur enim ab dolores ea",
+    },
+
+    {
+        name: "Silver Qiang",
+        imageUrl: "./imgs/Qiang.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nihil velit consequatur enim ab dolores ea",
+    },
+
+    {
+        name: "Silver Dao",
+        imageUrl: "./imgs/53a7f5bc8eef5133b63af57f3558169c.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nihil velit consequatur enim ab dolores ea",
+    },
+
+    {
+        name: "Silver Dao",
+        imageUrl: "./imgs/53a7f5bc8eef5133b63af57f3558169c.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nihil velit consequatur enim ab dolores ea",
+    },
+    {
+        name: "Silver Dao",
+        imageUrl: "./imgs/53a7f5bc8eef5133b63af57f3558169c.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nihil velit consequatur enim ab dolores ea",
+    },
+    {
+        name: "Silver Dao",
+        imageUrl: "./imgs/53a7f5bc8eef5133b63af57f3558169c.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nihil velit consequatur enim ab dolores ea",
+    },
+    {
+        name: "Silver Dao",
+        imageUrl: "./imgs/53a7f5bc8eef5133b63af57f3558169c.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nihil velit consequatur enim ab dolores ea",
+    },
+    
+];
+
+var tempWeaponPage;
+
+function PageCalculator() {
+    
+}
+
+var weaponsCardData = templateCard({
+    weapons
+});
+
+//0->7
+//8->15
+
+$("#weapon-list").html(weaponsCardData);
+
+
+// var weaponsPopUpData;
+// function ClosePopUp() {
+//     $(".pop-up").css("display", "none");
+// }
+
+// function OpenPopUp(name) {
+//     weapons.forEach(element => {
+//         if(element.name == name)
+//         {
+//             weaponsPopUpData = templatePopUp({
+//                 name: element.name,
+//                 imageUrl: element.imageUrl,
+//                 description: element.description
+//             });
+//             //TODO: Scroll height bug
+//             $("#weapon-pop-up").html(weaponsPopUpData);
+//         }
+//     });
+//     $(".pop-up").css("display", "flex");
+// }
+
+
+
+        
+
+        
+
+
+        
--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 
 
 var home = $("#Home");
-var category1 = $("#Category1");
+var category = $("#Category");
 var aboutUs = $("#About-Us");
-var category2 = $("#Category2");
 
 var homeBtn = $("#Home-Btn");
 var categoryBtn = $("#Category-Btn");
@@ -13,38 +12,82 @@ var categoryDown = $("#Category-Down");
 var categoryUp = $("#Category-Up");
 
 
-var selectLink = 0;
+
+
 
 function RemoveActive() {
-    category1.removeClass("active");
+    $("#Category .box:nth-child(1)").removeClass("active");
+    $("#Category .box:nth-child(2)").removeClass("active");
+
     aboutUs.removeClass("active");
     home.removeClass("active");
+    category.removeClass("active");
     categoryBtn.removeClass("active");
     aboutUsBtn.removeClass("active");
     homeBtn.removeClass("active");
-    category2.removeClass("active");
 }
 
-function AddActive(index) {
-    RemoveActive();
-    if(index == 0) {
-        home.addClass("active");
-        homeBtn.addClass("active");
+function ChangePage(pageType){
+    for (let i = 0; i < $(`.${pageType}`).length; i++) {
+        $(`.${pageType}`).eq(i).removeClass("active");
     }
-    else if(index == 1) {
-        category1.addClass("active");
-        categoryBtn.addClass("active");
+    console.log(pageCounter);
+    $(`.${pageType}`).eq(pageCounter).addClass("active");
+    // $(`.${pageType}`).eq(pageCounter).addClass("active");
 
-    }
-    else if(index == 2) {
-        aboutUs.addClass("active");
-        aboutUsBtn.addClass("active");
-
-    }
-    else if(index == 3) {
-        category2.addClass("active");
-        categoryBtn.addClass("active");
-    }
 }
+
+function PageUp(pageType) {
+    if (pageType == 'Category') {
+        pageCounter--;
+    }
+
+    if (pageType == 'Sword') {
+        pageCounter--;
+    }
+    ChangePage(pageType);
+}
+
+function PageDown(pageType) {
+    if (pageType == 'Category') {
+        pageCounter++;
+    }
+
+    if (pageType == 'Sword') {
+        pageCounter++;
+    }
+    ChangePage(pageType);
+}
+
+
+var currentPageType;
+var pageCounter = 0;
+
+// homeBtn.click(function() {
+//     pageCounter = 0;
+//     RemoveActive();
+//     $("#Home").addClass("active");
+//     homeBtn.addClass("active");
+//     currentPageType = 'Home';
+//     console.log(currentPageType);
+// });
+
+// categoryBtn.click(function() {
+//     pageCounter = 0;
+//     RemoveActive();
+//     $("#Category").eq(pageCounter).addClass("active");
+//     categoryBtn.addClass("active");
+//     currentPageType = 'Category';
+
+// });
+
+// aboutUsBtn.click(function() {
+//     pageCounter = 0;
+//     RemoveActive();
+//     $("#About-Us").addClass("active");
+//     currentPageType = 'AboutUs';
+//     aboutUsBtn.addClass("active");
+// });
+
 
 
