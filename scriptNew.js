5116ed537340d8a58b8b42cdb457ddd52ec6dc6f
Nick <1234lizhang@gmail.com>
2021-06-19 16:15:24 +0800

    New Design

---
+++ scriptNew.js
@@ -0,0 +1,14 @@
+// /// <reference path="./typings/globals/jquery/index.d.ts" />
+
+// console.log("Run");
+
+// $(".img").click(function () {
+//         console.log("pop");
+//         $(this).children(".pop-up").slideDown("fast");
+//     }, function() {
+//         $(this).children(".pop-up").slideUp("fast");
+//     }
+
+
+// );
+
