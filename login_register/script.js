287b8c1a2f4f7a239489c392ff8baf3177154d53
Nick <1234lizhang@gmail.com>
2021-06-25 17:46:54 +0800

    Login done

--- login_register/script.js
+++ login_register/script.js
@@ -7,32 +7,41 @@ const uInput = uField.querySelector("input");
 const pField = form.querySelector(".password");
 const pInput = pField.querySelector("input");
 
-// const form2 = document.getElementById("form2");
-// const ruField = form2.querySelector(".username");
-// const ruInput = ruField.querySelector("input");
-// const rpField = form2.querySelector(".password");
-// const rpInput = rpField.querySelector("input");
-// const rrpField = form2.querySelector(".repassword");
-// const rrpInput = rrpField.querySelector("input");
-// const reField = form2.querySelector(".email");
-// const reInput = reField.querySelector("input");
-// const overlay = document.getElementById("overlay");
+const form2 = document.getElementById("form2");
+const ruField = form2.querySelector(".username");
+const ruInput = ruField.querySelector("input");
+const rpField = form2.querySelector(".password");
+const rpInput = rpField.querySelector("input");
+const rrpField = form2.querySelector(".repassword");
+const rrpInput = rrpField.querySelector("input");
+const reField = form2.querySelector(".email");
+const reInput = reField.querySelector("input");
+const overlay = document.getElementById("overlay");
 
-document.querySelector(".login-button").addEventListener("click", () => {
-    console.log("Clicked");
-    document.querySelector(".login").classList.add("active");
+// document.querySelector(".login-button").addEventListener("click", () => {
+//     console.log("Login Click");
+//     document.querySelector(".login").classList.add("active");
+//     $(".register").removeClass("active");
+// });
+
+$(".login-button").click(function () {
+    $(".login").addClass("active");
+    $(".register").removeClass("active");
+
 });
 
-
-
-document.querySelector(".login_close").addEventListener("click", () =>{
-    document.querySelector(".login").classList.remove("active");
-    console.log("remove active");
+$(".register_open").click(function () {
+    $(".register").addClass("active")
+    $(".login").removeClass("active");
 });
 
-// $(".login-button").click(function () {
-//     $(".login").addClass("active");
-// })
+$(".login_close").click(function() {
+    $(".login").removeClass("active");
+})
+
+$(".register_close").click(function() {
+    $(".register").removeClass("active");
+})
 
 form.onsubmit = (u) =>{
     u.preventDefault();
@@ -81,82 +90,77 @@ form.onsubmit = (u) =>{
 // let errortext = eField.querySelector(".error-text");
 // (eInput.value != "")? errortext.innerText = "enter a valid email address" : errortext.innerText = "Email can't be blank"
 
-// document.querySelector(".register_open").addEventListener("click", () => {
-//     document.querySelector(".register").classList.add("active");
-// });
-// document.querySelector(".register_close").addEventListener("click", () =>{
-//     document.querySelector(".register").classList.remove("active");
-// });
 
-// form2.onsubmit = (e) =>{
-//     e.preventDefault();
-//     if(ruInput.value == ""){
-//         ruField.classList.add("shake", "error");
-//     }
-//     else{
-//         ruField.classList.remove("error");
-//     }
-//     if(rpInput.value == ""){
-//         rpField.classList.add("shake", "error");
-//     }
-//     else{
-//         rpField.classList.remove("error");
-//     }
-//     if(rrpInput.value == ""){
-//         rrpField.classList.add("shake", "error");
-//     }
-//     else{
-//         rrpField.classList.remove("error");
-//     }
-//     if(reInput.value == ""){
-//         reField.classList.add("shake", "error");
-//     }
-//     else if (!reInput.value == ""){
-//         check_email();
-//     }
-//     else{
-//         reField.classList.remove("error");
-//     }
-//     setTimeout(() =>{
-//         ruField.classList.remove("shake");
-//         rpField.classList.remove("shake");
-//         rrpField.classList.remove("shake");
-//         reField.classList.remove("shake");
-//     }, 500);
 
-//     function check_email(){
-//         let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-//         if(!reInput.value.match(pattern)){
-//             reField.classList.add("shake", "error");
-//             let errortext = reField.querySelector(".error-text");
-//             (reInput.value != "")? errortext.innerText = "enter a valid email address" : errortext.innerText = "Email can't be blank";
-//         }
-//         else{
-//             reField.classList.remove("error");
-//         }
-//     }
+form2.onsubmit = (e) =>{
+    e.preventDefault();
+    if(ruInput.value == ""){
+        ruField.classList.add("shake", "error");
+    }
+    else{
+        ruField.classList.remove("error");
+    }
+    if(rpInput.value == ""){
+        rpField.classList.add("shake", "error");
+    }
+    else{
+        rpField.classList.remove("error");
+    }
+    if(rrpInput.value == ""){
+        rrpField.classList.add("shake", "error");
+    }
+    else{
+        rrpField.classList.remove("error");
+    }
+    if(reInput.value == ""){
+        reField.classList.add("shake", "error");
+    }
+    else if (!reInput.value == ""){
+        check_email();
+    }
+    else{
+        reField.classList.remove("error");
+    }
+    setTimeout(() =>{
+        ruField.classList.remove("shake");
+        rpField.classList.remove("shake");
+        rrpField.classList.remove("shake");
+        reField.classList.remove("shake");
+    }, 500);
 
-//     // uInput.onkeuyp = ()=>{
-//     //     if(uInput.value == ""){
-//     //        uField.classList.add("error"); 
-//     //     }
-//     //     else{
-//     //         uField.classList.remove("error");
-//     //     }
-//     // }
+    function check_email(){
+        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+        if(!reInput.value.match(pattern)){
+            reField.classList.add("shake", "error");
+            let errortext = reField.querySelector(".error-text");
+            (reInput.value != "")? errortext.innerText = "enter a valid email address" : errortext.innerText = "Email can't be blank";
+        }
+        else{
+            reField.classList.remove("error");
+        }
+    }
 
-//     // pInput.onkeuyp = ()=>{
-//     //     if(pInput.value == ""){
-//     //        pField.classList.add("error"); 
-//     //     }        
-//     //     else{
-//     //         pField.classList.remove("error");
-//     //     }
-//     // }
+    // uInput.onkeuyp = ()=>{
+    //     if(uInput.value == ""){
+    //        uField.classList.add("error"); 
+    //     }
+    //     else{
+    //         uField.classList.remove("error");
+    //     }
+    // }
 
-//     if(!ruField.classList.contains("error") && !rpField.classList.contains("error") && 
-//     !rrpField.classList.contains("error") && !reField.classList.contains("error")){
-//         window.location.href = "#";
-//         console.log("submitted");
-//     }
-// }
+    // pInput.onkeuyp = ()=>{
+    //     if(pInput.value == ""){
+    //        pField.classList.add("error"); 
+    //     }        
+    //     else{
+    //         pField.classList.remove("error");
+    //     }
+    // }
+
+    if(!ruField.classList.contains("error") && !rpField.classList.contains("error") && 
+    !rrpField.classList.contains("error") && !reField.classList.contains("error")){
+        window.location.href = "#";
+        console.log("submitted");
+    }
+}
